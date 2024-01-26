94f925316f1b2f69a8d3f7104108d59014315b5b
Fernando Val <aaromnido@gmail.com>
2024-01-26 17:34:48 +0100

    Move the Cookies banner to the index page from the Layout

--- .netlify/functions-internal/ssr/chunks/generic_AfNDE-lw.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/generic_l1Aqw5Ym.mjs');
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/signup-kit-digital_Cn4EYl4H.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/signup-kit-digital_UNibhUAe.mjs');
-
-export { page };
