2deb3e0cc11aaf1c9799c123a7042f590dc80969
Fernando Val <aaromnido@gmail.com>
2024-01-29 10:48:01 +0100

    Implement Alpine JS

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from '@astrojs/vercel/serverless';
 
+import alpinejs from "@astrojs/alpinejs";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), alpinejs()],
   output: "hybrid",
-  adapter: vercel(),
+  adapter: vercel()
 });
