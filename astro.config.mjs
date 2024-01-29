39c526e2a55ac0a3b2a0421301f8db0ca4525505
Fernando Val <aaromnido@gmail.com>
2024-01-29 15:42:54 +0100

    Uninstalling Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
 
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), alpinejs()],
-  output: "hybrid",
-  adapter: vercel()
+  output: "hybrid"
 });
