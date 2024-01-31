2e14280e0c5080cbe6308140845c730b5f5a1fc6
Fernando Val <aaromnido@gmail.com>
2024-01-31 18:10:04 +0100

    Amking a build to vercel

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/index_dAHIdTju.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./prerender_-ezvhKq8.mjs').then(n => n.i);
-
-export { page };
---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/signup-kit-digital_90jpU6YT.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./prerender_mYJgWfuY.mjs').then(n => n.s);
+
+export { page };
